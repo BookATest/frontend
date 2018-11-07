152ec73e7f2f2cc5cfd540f19f50db8a95aa0429
Matt Inamdar <matt@ayup.agency>
2018-11-07 17:11:03 +0000

    added name input

--- src/views/make-booking/UserDetails.vue
+++ src/views/make-booking/UserDetails.vue
@@ -8,14 +8,40 @@
     </bat-text-header>
 
     <bat-content>
-      <p>lorem</p>
+      <div class="form">
+
+        <bat-field text>
+          <label for="name">
+            Please enter your name
+            <span class="instructions">(doesn't have to be your real name)</span>
+          </label>
+          <bat-text-input v-model="name" id="name" />
+        </bat-field>
+
+      </div>
     </bat-content>
 
   </div>
 </template>
 
 <script>
+import Field from '@/components/Field';
+import FieldInput from '@/components/FieldInput';
+import TextInput from '@/components/TextInput';
+
 export default {
   name: 'UserDetails',
+
+  components: {
+    BatField: Field,
+    BatFieldInput: FieldInput,
+    BatTextInput: TextInput,
+  },
+
+  data() {
+    return {
+      name: '',
+    };
+  },
 };
 </script>
