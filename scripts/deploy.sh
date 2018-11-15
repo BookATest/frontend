dae611997d23ef8dc30cd780d4cebe044f8365b9
Matt Inamdar <matt@ayup.agency>
2018-11-15 13:11:50 +0000

    set aws credentials

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+# Set AWS CLI credentials.
+echo "Setting AWS credentials..."
+touch ~/.aws/credentials
+cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+
+# Set AWS CLI config.
+echo "Setting AWS config..."
+touch ~/.aws/config
+cat > ~/.aws/config << EOF
+[default]
+region = $AWS_REGION
+output = $AWS_DEFAULT_OUTPUT
+EOF
+
 # Get the .env file.
 echo "Downloading .env file..."
 aws secretsmanager get-secret-value \
