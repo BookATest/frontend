8022074e6b3b76815843626b833fe2bfd4b04138
Matt Inamdar <matt@ayup.agency>
2018-11-16 10:21:15 +0000

    fixed multiline regex

--- src/components/PostcodeInput.vue
+++ src/components/PostcodeInput.vue
@@ -25,15 +25,23 @@ export default {
     },
   },
 
+  data() {
+    return {
+      postcodeRegex: new RegExp(
+        [
+          '^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][',
+          'A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z]',
+          '[A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\\s?[0-9][A-Za-z]{2})$',
+        ].join(''),
+        'g',
+      ),
+    };
+  },
+
   computed: {
     valid() {
       const postcode = this.value || '';
-      return postcode.match(new RegExp([
-        '/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})',
-        '|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z]',
-        '[0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]',
-        '))))\s?[0-9][A-Za-z]{2})$/g',
-      ].join('')));
+      return postcode.match(this.postcodeRegex);
     },
   },
 
