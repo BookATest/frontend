afacd626322a26717b30b126da5f3890aeb494ab
Matt Inamdar <matt@ayup.agency>
2018-11-26 14:59:54 +0000

    using three selects for date input

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -1,20 +1,74 @@
 <template>
-  <input
-    :value="value"
-    @input="onInput"
-    type="date"
-    class="input input--date"
-    placeholder="DD/MM/YYYY"
-  >
+  <div>
+
+    <div class="flex-container flex-container--flush flex-container--wrap">
+      <bat-select-input
+        :value="day"
+        :options="days | options"
+      />
+
+      <bat-select-input
+        :value="month"
+        :options="months | options"
+      />
+    </div>
+
+    <bat-select-input
+      :value="year"
+      :options="years | options"
+    />
+
+  </div>
 </template>
 
 <script>
+import SelectInput from '@/components/SelectInput';
+
 export default {
   name: 'DateInput',
+
+  components: {
+    BatSelectInput: SelectInput,
+  },
 
   props: {
     value: {
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      year: null,
+      month: null,
+      day: null,
+    };
+  },
+
+  computed: {
+    days() {
+      return [...Array(31).keys()].map((day) => ++day);
+    },
+
+    months() {
+      return [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+    },
+
+    years() {
+      return [...Array(201).keys()].map((day) => day += new Date().getFullYear() - 100);
     },
   },
 
@@ -23,5 +77,23 @@ export default {
       this.$emit('input', event.target.value);
     },
   },
+
+  filters: {
+    options(values) {
+      return values.map((value) => ({ value, text: value }));
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.flex-container {
+  &--flush {
+    padding: 0;
+  }
+
+  &--wrap {
+    flex-wrap: nowrap;
+  }
+}
+</style>
