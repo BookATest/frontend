886e1174eddebea69ee88d41545f5d46807f101a
Matt Inamdar <matt@ayup.agency>
2018-12-06 16:40:45 +0000

    showed directions by default

--- src/views/make-booking/Confirmation.vue
+++ src/views/make-booking/Confirmation.vue
@@ -118,7 +118,7 @@ export default {
       appointment: new Appointment().get,
       clinic: new Clinic().get,
       userDetails: new UserDetails().get,
-      showDirections: false,
+      showDirections: true,
       settings: new Settings().load(),
     };
   },
