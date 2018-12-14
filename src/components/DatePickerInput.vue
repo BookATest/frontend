70d72b18ac24cde29d711c0166e7c0895a42cf5c
Matt Inamdar <matt@ayup.agency>
2018-12-14 13:17:39 +0000

    disabled dates with no appointments available

--- src/components/DatePickerInput.vue
+++ src/components/DatePickerInput.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <bat-field date-picker>
-      
-      <a @click.prevent="toggleDatePicker" href="#">
+
+      <bat-loader v-if="loading" />
+      <a v-else @click.prevent="toggleDatePicker" href="#">
         <strong>Choose another date?</strong>
       </a>
-      
+
     </bat-field>
 
     <date-picker
       ref="datePicker"
       monday-first
+      :disabled-dates="disabledDates"
       input-class="date-picker--hidden"
       @selected="onDateSelected"
     />
@@ -21,6 +23,7 @@
 import moment from 'moment';
 import Datepicker from 'vuejs-datepicker';
 import Field from '@/components/Field';
+import Clinic from '@/utilities/Clinic';
 
 export default {
   name: 'DatePickerInput',
@@ -38,11 +41,27 @@ export default {
 
   data() {
     return {
+      clinicCache: new Clinic(),
+      appointments: [],
+      loading: false,
       today: moment(),
       tomorrow: moment().add(1, 'day'),
       dayPlus2: moment().add(2, 'days'),
       dayPlus3: moment().add(3, 'days'),
       html5DateFormat: moment.HTML5_FMT.DATE,
+      disabledDates: {
+        customPredictor: (date) => {
+          const checkDate = moment(date)
+
+          const appointmentExists = this.appointments.find((appointment) => {
+            const appointmentDate = moment(appointment.start_at, moment.ISO_8601);
+
+            return checkDate.format(moment.HTML5_FMT.DATE) === appointmentDate.format(moment.HTML5_FMT.DATE);
+          });
+
+          return appointmentExists === undefined;
+        }
+      }
     };
   },
 
@@ -58,7 +77,23 @@ export default {
     toggleDatePicker() {
       this.$refs.datePicker.showCalendar();
     },
+
+    async fetchAppointments() {
+      this.loading = true;
+
+      this.appointments = await this.fetchAll('/v1/appointments', {
+        'filter[clinic_id]': this.clinicCache.get.id,
+        'filter[available]': true,
+        'sort': 'start_at',
+      });
+
+      this.loading = false;
+    }
   },
+
+  created() {
+    this.fetchAppointments();
+  }
 };
 </script>
 
